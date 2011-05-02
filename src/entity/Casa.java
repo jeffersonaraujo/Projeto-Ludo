7f788dcd3c8b7d7c0c1db1050f4d9b9efc6a5565
Jefferson Araujo <jefferssonaraujo@gmail.com>
2011-05-02 00:55:20 -0300

    Commit dos atributos e métodos que compõem uma casa do tabuleiro. Método comportamentoCasa e os atributos String Id e IComportamento comportamento.

--- src/entity/Casa.java
+++ src/entity/Casa.java
@@ -1,5 +1,7 @@
 package entity;
 
+import strategy.IComportamento;
+
 /**
  * Class Casa
  *
@@ -9,4 +11,53 @@ package entity;
  * Copyright (C) 2011 Equipe Bangladesh.
  */
 public class Casa {
+
+    private String id;
+    private IComportamento comportamento;
+
+    /**
+     * Construtor da classe Casa
+     * 
+     * @param id Identificado da casa.
+     * @param comportamento Comportamento que a casa vai executar.
+     */
+    public Casa(String id, IComportamento comportamento) {
+        this.id = id;
+        this.comportamento = comportamento;
+    }
+
+    /**
+     * Método que executa o comportamento da casa no tabuleiro.
+     * 
+     * @return o nome referente ao comportamento da casa.
+     */
+    public String comportamentoCasa() {
+        return comportamento.comportamentoCasa();
+    }
+
+    /**
+     * getComportamento()
+     * 
+     * @return o comportamento da casa no tabuleiro.
+     */
+    public IComportamento getComportamento() {
+        return comportamento;
+    }
+
+    public void setComportamento(IComportamento comportamento) {
+        this.comportamento = comportamento;
+    }
+
+    /**
+     * getId()
+     * 
+     * @return o idendificador da casa. 
+     */
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
